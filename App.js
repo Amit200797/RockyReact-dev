159e66925c37963b4743f84b058d4395a4deeddf
Amit200797 <88579805+Amit200797@users.noreply.github.com>
2024-10-02 14:39:23 +0530

    updated code

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"This is  a React Development"),
-    React.createElement("h2",{},"I am h2 tag")
-]),
-    React.createElement("div",{id:"parent"},[
-        React.createElement("div",{id:"child"}),
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
-    ]),
-]);
+//Component Compositions
 
+/* Header 
+-LoGO component
+-Nav Items
+Body 
+-Search Bar
+*Restorent container
+*Restorent card
+Footer
+-copyright
+-Link
+-Address
+-Conatct
 
+ */
 
-    console.log(parent);
+const element =  <span>React Element</span> 
+const Title =  () => (
+    <h1 className="head" tabIndex="6">
+        {element}
+        Namaste React -Dev
+        </h1>
+    
+    );
+const HeadingComponent = () =>(
+    <div id = "container">
+    
+   <Title />
+   {Title()}
+   <Title></Title>
+    <h1 className="Heading">Namste react functional component</h1> 
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
